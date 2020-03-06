38da12c2aacd4d0d7e92bc40b25e847bdd86ec90
NULL-Response <5726382+nullresponse@user.noreply.gitee.com>
2020-03-06 22:03:16 +0800

    rename funcpro.h to headers_and_prototypes.h.

---
+++ headers_and_prototypes.h
@@ -0,0 +1,18 @@
+#ifndef HEADERS_AND_PROTOTYPES_H_INCLUDED
+#define HEADERS_AND_PROTOTYPES_H_INCLUDED
+
+//header files
+#include<iostream>
+
+//function prototypes
+bool Is_Leap_Year(const int &y);
+void instruction();
+void func0();
+void func1();
+void func2();
+void func3();
+void func4();
+void func5();
+void func6();
+
+#endif // HEADERS_AND_PROTOTYPES_H_INCLUDED
